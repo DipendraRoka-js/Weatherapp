faccdf879c9e0eb76c6fca5c618e1a2d177d402b
DipendraRoka-js <np03cs4a230497@heraldcollege.edu.np>
2026-07-25 21:01:36 +0545

    Add Enter key functionality for search and fix fetch URL in weather retrieval

--- index.js
+++ index.js
@@ -20,11 +20,17 @@ searchBtn.addEventListener("click", () => {
     return;
   }
 
+  cityInput.addEventListener("keydown",()=>{
+    if(event.key==="Enter"){
+      searchBtn.click();
+    }
+  });
+
   getWeather(city);
 });
 
 async function getWeather(city) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  const url = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
 
   try {
     const response = await fetch(url);
